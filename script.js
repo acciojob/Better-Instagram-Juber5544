24a5c66be2e8cfa01c140e775e5d87b58b5b7918
Juber5544 <117826480+Juber5544@users.noreply.github.com>
2025-02-03 13:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+<div class="demo">
+
+		This is a demo of box model try to replicate this box model in Html and Css and try to pass all the test cases.
+
+	</div>
+            <img id="drag5" src="https://picsum.photos/200/300.jpg" draggable="true">
+
+        </div>
+
+        <div id="div6" class="image-box">
+
+            <img id="drag6" src="https://picsum.photos/id/102/200/300" draggable="true">
+
+        </div>
+
+    </div>
+	  <script>
+		    let draggedImage = null;
+    function allowDrop(event){
+        event.preventDefault();
+    }
+    function drag(event){
+        draggedImage = event.target;
+    }
+
+    function drop(event){
+        event.preventDefault();
+        const targetContainer = event.currentTarget;
+
+       if(targetContainer !== draggedImage.parentElement){
+        const targetImage = targetContainer.querySelector('img');
+
+        const tempSrc = targetImage.src;
+        targetImage.src = draggedImage.src;
+        draggedImage.src = tempSrc;
+       }
+    }
